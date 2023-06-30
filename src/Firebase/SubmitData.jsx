0375c78c3cd18019cd83a8f8fc25b099c1c7519a
Yunus Emre <dmrlyunusemre@gmail.com>
2023-06-30 15:50:28 +0300

    30.06.2023

--- src/Firebase/SubmitData.jsx
+++ src/Firebase/SubmitData.jsx
@@ -5,6 +5,7 @@ import {addDoc,collection} from "@firebase/firestore"
 
 export default function SubmitData() {
     const userRef = useRef();
+    const emailRef = useRef();
     const ref = collection (firestore,"users");
 
     const handleSave = async(e) =>
@@ -12,7 +13,7 @@ export default function SubmitData() {
         e.preventDefault();
         console.log(userRef.current.value);
 
-        let data = {userName:userRef.current.value};
+        let data = {userName:userRef.current.value,email:emailRef.current.value};
 
         try{
             addDoc(ref,data);
@@ -22,15 +23,34 @@ export default function SubmitData() {
         } 
     };
   return (
-    <div>
-        <form onSubmit={handleSave}>
-            <label>
-                Enter User Name:
-            </label>
-            <input type="text" ref={userRef}>
-            </input>
-            <button type="submit">Click</button>
+    <div style={{background: 'linear-gradient(to right, #353435, #cf2627, #353435)',height:'100vh'}}>
+        <form style={{display:'flex',flexDirection:'column'}} onSubmit={handleSave}>
+            <div style={{backgroundColor:'white',width:'20vw',height:'50vh',alignSelf:'center',marginTop:'20vh',borderRadius:'40px'}}>
+            <img src={require('../img/homecooking-logo.png')} style={{width:'12vw',marginLeft:'4vw',marginTop:'2vh'}} alt="Resim açıklaması"/>
+                <div>
+                <h1 style={{textAlign:'center'}}>Web Panel</h1>
+                </div>
+                <div>
+                <label>
+                    Enter User Name:
+                </label>
+                <input type="text" ref={userRef}></input>
+            </div>
+            <div>
+                <label>
+                    Enter Email:
+                </label>
+                <input type="text" ref={emailRef}>
+                </input>
+            </div>
+            <div>
+                 <button type="submit">Click</button>
+            </div>
+            </div>
         </form>
     </div>
   )
 }
+
+
+
